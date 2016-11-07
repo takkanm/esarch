17a32e874499f1c8392fbe4c7939054bda4cf747
takkanm <takkanm@gmail.com>
2016-11-07 23:17:15 +0900

    add usage

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,8 +1,10 @@
 defmodule CLI do
   def main(args) do
-    option_strict = [add: :boolean, page: :integer]
+    option_strict = [add: :boolean, page: :integer, help: :boolean]
 
     case OptionParser.parse(args, switches: option_strict) do
+      {[help: true], _, _} ->
+        usage
       {[add: true], [organization, token], _} ->
         Esarch.add(organization, token)
       {[], [organization|keywords], _} ->
@@ -16,6 +18,6 @@ defmodule CLI do
   end
 
   def usage do
-    IO.puts("hi")
+    IO.puts "esarch [--page N] organization_name keyword1 [keyword2 keyword3 ...]"
   end
 end
